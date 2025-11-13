226d19bee940aa482520dd42fcfbd102447314a9
Owen Mao <owenmao@mac.lan>
2025-11-12 17:35:40 -0800

    updated TODO.md

--- Backend/app/routes/auth.js
+++ Backend/app/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+const User = require("../models/User");
 //const bodyParser = require('body-parser');
 //const jwt = require('jsonwebtoken');
 const router = express.Router();
-
-
-// Secret key for JWT (keep this safe in env variables!)
-//const SECRET_KEY = "supersecretkey123";
+dotenv.config();
 
 /**
  * @swagger
@@ -41,8 +42,25 @@ const router = express.Router();
  *                   type: string
  *                   example: client1234token
  */
-router.post("/api/login", (req, res) => {
-  res.json({ token: "client1234token" });
+router.post("/api/login", async (req, res) => {
+  try {
+    const {username, password} = req.body;
+    const user = await User.findOne({username: username});
+    if(!user) return res.status(400).json({message: "Invalid username"});
+    console.log(password);
+    console.log(user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({message: "Incorrect password"});
+
+    const token = jwt.sign(
+      {userId: user._id, username: user.username}, 
+      process.env.JWT_SECRET, 
+      {expiresIn: "24h"}
+    );
+    res.json({ token, user: { id: user._id, username: user.username}});
+  } catch (err){
+    res.status(500).json({ message: "Login Failed", error: err.message});
+  }
 });
 
 /**
@@ -60,7 +78,7 @@ router.post("/api/login", (req, res) => {
  *         description: Unauthorized
  */
 router.post("/api/logout", (req, res) => {
-  res.sendStatus(200);
+  res.status(200);
 });
 
 module.exports = router;
