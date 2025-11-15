ead524f41ac1f654757f267a23f7154e7d078030
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2025-11-14 17:27:23 -0800

    router + navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,15 +1,29 @@
 <template>
-
-  <LoginScreen/>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
-  import LoginScreen from './components/login.vue'
+import Navbar from './components/Navbar.vue'
 
-  export default{
-    name: 'App',
-    components: {
-      LoginScreen 
-    }
+export default {
+  name: 'App',
+  components: {
+    Navbar
   }
+}
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Arial, sans-serif;
+}
+</style>
